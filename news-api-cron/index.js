5d316e17ba2b3a87c0f04da8ae4ff5b1b2678cd5
GineTik <densheva18072003@gmail.com>
2025-05-29 20:24:40 +0300

    Update cron

--- news-api-cron/index.js
+++ news-api-cron/index.js
@@ -4,15 +4,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const fetchNews = async () => {
+  const response = await axios.get(process.env.FETCH_NEWS_URL);
+
+  console.log("Send data");
+  if (response.status === 200) {
+    await axios.post(process.env.URL_FOR_UPLOAD_NEWS, response.data.articles);
+  }
+  return response.data;
+};
+
+await fetchNews();
+
 cron.schedule("*/30 * * * *", async () => {
   console.log("running a task every 30 minutes");
   try {
-    const response = await axios.get(process.env.FETCH_NEWS_URL);
-
-    console.log("Send data");
-    if (response.status === 200) {
-      await axios.post(process.env.URL_FOR_UPLOAD_NEWS, response.data.articles);
-    }
+    await fetchnews();
   } catch (error) {
     console.error(error);
   }
